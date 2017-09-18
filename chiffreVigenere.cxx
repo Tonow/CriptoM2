2c0dcf3f36dc3c428cc7a5ea7f12033865373ad6
mugt <nwck.thomas@gmail.com>
2017-09-18 10:29:52 +0200

    Utilisation d'un ficher pour chiffre vignenere

---
+++ chiffreVigenere.cxx
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "utils.h"
+
+using namespace std;
+
+
+std::string chiffreVigenere(const std::string & clair , const std::string & cle )
+{
+    int longueurCle = cle.size();
+
+
+    cout << "La chaine contient : " << clair << endl;
+
+    std::string chiffre;
+    return chiffre;
+}
--- filter-az.cxx
+++ filter-az.cxx
@@ -11,20 +11,6 @@
 
 using namespace std;
 
-std::string chiffreVigenere(const std::string & clair , const std::string & cle )
-{
-    int longueurCle = cle.size();
-
-    cout << "Longueur de la chaine : " << longueurCle;
-
-    //boost::to_upper(clair);
-    //std::string chiffre = boost::to_upper_copy<std::string>(clair);
-
-    cout << "La chaine contient : " << clair << endl;
-
-    std::string chiffre;
-    return chiffre;
-}
 
 int main( int argc, char** argv )
 {
